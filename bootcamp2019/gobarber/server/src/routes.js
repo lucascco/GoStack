a582fa5ecbfac27379b2efd72525a5316b265b3f
Lucas C Correa <lucasccorrea@gmail.com>
2019-06-29 16:19:52 -0300

    add index to class Database

--- bootcamp2019/gobarber/server/src/routes.js
+++ bootcamp2019/gobarber/server/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json('Hello Word');
+routes.get('/', async (req, res) => {
+  const user = User.create({
+    name: 'Lucas Carvalho Correa',
+    email: 'lucasccorrea@gmail.com',
+    password_hash: '123456789',
+  });
+  res.json(user);
 });
 
 export default routes;
