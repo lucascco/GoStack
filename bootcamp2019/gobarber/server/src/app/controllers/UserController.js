0972384fba9828e47e726a7d87d9d66fde96a229
Lucas C Correa <lucasccorrea@gmail.com>
2019-06-30 16:28:30 -0300

    Add middleware auth, update User

--- bootcamp2019/gobarber/server/src/app/controllers/UserController.js
+++ bootcamp2019/gobarber/server/src/app/controllers/UserController.js
@@ -15,6 +15,28 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+
+    if (user.email !== email) {
+      const userExist = await User.findOne({ where: { email } });
+      if (userExist) {
+        return res
+          .status(400)
+          .json({ error: 'Email already exist to another user!' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(400).json({ error: 'User password does not match!' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, email, provider });
+  }
 }
 
 export default new UserController();
