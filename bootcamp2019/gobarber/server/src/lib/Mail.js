aa482685f7c9460efd62027e422f91a246380611
Lucas C Correa <lucasccorrea@gmail.com>
2019-07-27 00:15:50 -0300

    add config to use template in nodemailer

--- bootcamp2019/gobarber/server/src/lib/Mail.js
+++ bootcamp2019/gobarber/server/src/lib/Mail.js
@@ -1,4 +1,7 @@
 import nodemailer from 'nodemailer';
+import { resolve } from 'path';
+import expresshbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
 import mailConfig from '../config/mail';
 
 class Mail {
@@ -10,6 +13,24 @@ class Mail {
       secure,
       auth: auth.user ? auth : null,
     });
+    this.configureTemplate();
+  }
+
+  configureTemplate() {
+    const viewsPath = resolve(__dirname, '..', 'app', 'views', 'email');
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: expresshbs.create({
+          layoutsDir: resolve(viewsPath, 'layouts'),
+          partialsDir: resolve(viewsPath, 'partials'),
+          defaultLayout: 'default',
+          extname: '.hbs',
+        }),
+        viewPath: viewsPath,
+        extName: '.hbs',
+      })
+    );
   }
 
   sendEmail(message) {
